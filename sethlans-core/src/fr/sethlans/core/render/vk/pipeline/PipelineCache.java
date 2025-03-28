12a30df9af03db7dcd492c353a13a6ae7cab02a6
GnosticOccultist <41146201+GnosticOccultist@users.noreply.github.com>
2025-03-28 20:56:20 +0100

    Retrieve pipeline cache data
    * Create PipelineCache from initial data.

--- sethlans-core/src/fr/sethlans/core/render/vk/pipeline/PipelineCache.java
+++ sethlans-core/src/fr/sethlans/core/render/vk/pipeline/PipelineCache.java
@@ -1,5 +1,7 @@
 package fr.sethlans.core.render.vk.pipeline;
 
+import java.nio.ByteBuffer;
+
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.vulkan.VK10;
 import org.lwjgl.vulkan.VkPipelineCacheCreateInfo;
@@ -14,17 +16,33 @@ public class PipelineCache {
     private long handle = VK10.VK_NULL_HANDLE;
 
     public PipelineCache(LogicalDevice device) {
+        this(device, null);
+    }
+
+    public PipelineCache(LogicalDevice device, ByteBuffer cacheData) {
         this.device = device;
 
         try (var stack = MemoryStack.stackPush()) {
             var cacheCreateInfo = VkPipelineCacheCreateInfo.calloc(stack)
-                    .sType(VK10.VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO);
+                    .sType(VK10.VK_STRUCTURE_TYPE_PIPELINE_CACHE_CREATE_INFO)
+                    .pInitialData(cacheData);
 
             var pHandle = stack.mallocLong(1);
             var err = VK10.vkCreatePipelineCache(device.handle(), cacheCreateInfo, null, pHandle);
             VkUtil.throwOnFailure(err, "create pipeline cache");
             this.handle = pHandle.get(0);
         }
+    }
+
+    public ByteBuffer getData(MemoryStack stack) {
+        var pCacheSize = stack.mallocPointer(1);
+        VK10.vkGetPipelineCacheData(device.handle(), handle, pCacheSize, null);
+
+        var size = (int) pCacheSize.get(0);
+        var cacheData = stack.malloc(size);
+        VK10.vkGetPipelineCacheData(device.handle(), handle, pCacheSize, cacheData);
+
+        return cacheData;
     }
 
     long handle() {
